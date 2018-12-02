3e43719e1c341dee70c05f195aabc0c56aa963dd
Rhett Del Fierro <thinquetank@gmail.com>
2018-12-02 13:42:08 -0800

    completed my version steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,18 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+const getNumberOfSpaces = (step, spaces) => spaces - step
+const createSteps = (steps) => [...Array(steps).keys()].map(_ => '#').join('')
+const createSpaces = (numberOfSpaces) => [...Array(numberOfSpaces).keys()].map(_ => ' ').join('')
+function steps(n) {
+    const ints = [...Array(n).keys()].map(i => i + 1) // [1,2,3,4,5...]
+    const arr = ints.reduce((acc,val) => {
+        const steps = createSteps(val)
+        const numberOfSpaces = getNumberOfSpaces(val, n)
+        const spaces = createSpaces(numberOfSpaces)
+        return [...acc, steps + spaces]
+    }, [])
+    arr.forEach(console.log)
+}
 
 module.exports = steps;
