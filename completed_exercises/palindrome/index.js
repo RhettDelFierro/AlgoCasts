815d3d9914b7a2ce8978712e1dd372886c7ca395
Rhett Del Fierro <thinquetank@gmail.com>
2018-12-02 10:30:58 -0800

    completed palindome

--- completed_exercises/palindrome/index.js
+++ completed_exercises/palindrome/index.js
@@ -7,7 +7,12 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// this is not very efficient: once we reach the center, we don't have to check the remaining elements, but .every() will due to str[str.length - i - 1] here.
+// however, it still works.
 function palindrome(str) {
+  // used to do a boolean check on every element in the array
+  // the callback function is invoked for every element in the array.
+     // if any callback returns false, then the whole result of arr.every(..) will return false. Otherwise .every() returns true.
   return str.split('').every((char, i) => {
     return char === str[str.length - i - 1];
   });
@@ -15,6 +20,7 @@ function palindrome(str) {
 
 module.exports = palindrome;
 
+// first solution:
 // function palindrome(str) {
 //   const reversed = str
 //     .split('')
