4e1566ade420fa8c47d9a319646a34fc126adaa1
Rhett Del Fierro <thinquetank@gmail.com>
2018-12-02 09:40:10 -0800

    finished reversestring

--- completed_exercises/reversestring/index.js
+++ completed_exercises/reversestring/index.js
@@ -12,6 +12,7 @@ function reverse(str) {
 
 module.exports = reverse;
 
+// first solution
 // function reverse(str) {
 //   return str
 //     .split('')
@@ -19,6 +20,14 @@ module.exports = reverse;
 //     .join('');
 // }
 
+
+// second solution
+/*
+  1. Create an empty string called 'reversed'
+  2. for each character in the provided string:
+     a. take the character and add it to the start of 'reversed'
+  3. return the vairable 'reversed'  
+*/
 // function reverse(str) {
 //   let reversed = '';
 //
